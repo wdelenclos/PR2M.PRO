f78443d1d1e9226e7341665a06586116478405ab
WDelenclos <wdelenclos@gmail.com>
2017-09-09 17:20:50 +0200

    Fix

---
+++ app/template/content_shared3.php
@@ -0,0 +1,31 @@
+
+<!-- page content -->
+<div class="right_col" role="main">
+    <!-- top tiles -->
+    <div class="clearfix"></div>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="x_panel">
+                <div class="x_title">
+                    <h2>Objectifs</h2>
+                    <ul class="nav navbar-right panel_toolbox">
+                        <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                        </li>
+                    </ul>
+                    <div class="clearfix"></div>
+                </div>
+                <div class="x_content">
+                    <div class="col-md-12">
+                        <p>Selon nos questions cliniques et notre protocole d&rsquo;entra&icirc;nement &agrave; la d&eacute;nomination chronom&eacute;tr&eacute;e d&rsquo;images, nous souhaitons mesurer les effets sur&nbsp;:</p>
+                        <p>&nbsp;1) l&rsquo;acc&egrave;s lexical,</p>
+                        <p>&nbsp;2) les diff&eacute;rentes modalit&eacute;s de vitesse de traitement verbal et non verbal.</p>
+                        <h3>Crit&egrave;res d&rsquo;&eacute;valuation principaux :</h3>
+                        <p>Nous &eacute;valuerons nos objectifs gr&acirc;ce aux r&eacute;sultats obtenus en vitesse et en pr&eacute;cision aux &eacute;preuves, avant et apr&egrave;s entra&icirc;nement.&nbsp;</p>
+                        <p>La ligne de base nous permettra &eacute;galement de comparer les performances des deux groupes: contr&ocirc;le (CR) et exp&eacute;rimental (XP).&nbsp;</p>
+                        <p>&nbsp;</p></div>
+            </div>
+        </div>
+    </div>
+    <br/>
+</div>
+<!-- /page content -->
