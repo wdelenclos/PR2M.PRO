379211a463c97d5090a5a5724b2e770dc6b5f0d7
WDelenclos <wdelenclos@gmail.com>
2017-09-09 11:53:25 +0200

    Dashboard Guide

--- app/template/content_dashboard.php
+++ app/template/content_dashboard.php
@@ -35,10 +35,125 @@
 
     <div class="clearfix"></div>
     <div class="row">
+
         <div class="col-md-6 col-sm-6 col-xs-12">
             <div class="x_panel">
                 <div class="x_title">
-                    <h2><i class="fa fa-info"></i> Bienvenue sur PR2M !</h2>
+                    <h2><i class="fa fa-compass "></i>Guide d'utilisation <small>Obtenez de l'aide facilement</small></h2>
+                    <ul class="nav navbar-right panel_toolbox">
+                        <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                        </li>
+                    </ul>
+                    <div class="clearfix"></div>
+                </div>
+                <div class="x_content">
+
+                    <div class="" role="tabpanel" data-example-id="togglable-tabs">
+                        <ul id="myTab1" class="nav nav-tabs bar_tabs right" role="tablist">
+                            <li role="presentation" class="active"><a href="#tab_content11" id="home-tabb" role="tab" data-toggle="tab" aria-controls="home" aria-expanded="true">PR2M, c'est quoi ?</a>
+                            </li>
+                            <li role="presentation" class=""><a href="#tab_content22" role="tab" id="profile-tabb" data-toggle="tab" aria-controls="profile" aria-expanded="false">Comment je l'utilise ?</a>
+                            </li>
+                            <li role="presentation" class=""><a href="#tab_content33" role="tab" id="profile-tabb3" data-toggle="tab" aria-controls="profile" aria-expanded="false">Calendrier</a>
+                            </li>
+                            <li role="presentation" class=""><a href="#tab_content44" role="tab" id="profile-tabb3" data-toggle="tab" aria-controls="profile" aria-expanded="false">Contact</a>
+                            </li>
+                        </ul>
+                        <div id="myTabContent2" class="tab-content">
+                            <div role="tabpanel" class="tab-pane fade active in" id="tab_content11" aria-labelledby="home-tab">
+                                <p>PR2M, c&rsquo;est avant tout un travail de m&eacute;moire cherchant &agrave; valider un Protocole de Rem&eacute;diation du Manque du Mot. Soutenue par Gilles Leloup (Dr. en Sciences du langage, orthophoniste) et Anne Bragard (Dr. en Sciences psychologiques et de l&rsquo;&eacute;ducation, logop&egrave;de) cette &eacute;tude a pour objectif de valider scientifiquement un protocole facilement op&eacute;rationnel en clinique.</p>
+                                <p>PR2M, c&rsquo;est aussi une plateforme cr&eacute;&eacute;e dans le cadre du projet. Elle permet ainsi de diffuser le protocole et de r&eacute;colter rapidement les donn&eacute;es. Interface entre les praticiens et les organisateurs de l&rsquo;&eacute;tude, elle est un moyen de gestion &agrave; distance privil&eacute;gi&eacute; pour r&eacute;unir le maximum de collaborateurs francophones.</p>
+                                <p><em>Pour en savoir plus sur l&rsquo;&eacute;tude, allez dans l&rsquo;onglet &laquo;&nbsp;Donn&eacute;es partag&eacute;es&nbsp;&raquo;. </em></p>
+                            </div>
+                            <div role="tabpanel" class="tab-pane fade" id="tab_content22" aria-labelledby="profile-tab">
+                                <p>La plateforme se veut fonctionnelle et agr&eacute;able d&rsquo;utilisation.</p>
+                                <p>Le <strong>tableau de bord</strong> vous permet de voir directement&nbsp; la liste des patients et l&rsquo;&eacute;tat&nbsp; d&rsquo;avancement&nbsp; des passations.</p>
+                                <p>L&rsquo;onglet <strong>Patients</strong> vous permet&nbsp;:</p>
+                                <ul>
+                                    <li>de cr&eacute;er un nouveau patient</li>
+                                    <li>d&rsquo;acc&eacute;der &agrave; la liste compl&egrave;te de ceux d&eacute;j&agrave; cr&eacute;es.</li>
+                                </ul>
+                                <p>L&rsquo;onglet <strong>Tests</strong> permet&nbsp; d&rsquo;acc&eacute;der&nbsp;:</p>
+                                <ul>
+                                    <li>&agrave; l&rsquo;ordre de passation</li>
+                                    <li>au mat&eacute;riel</li>
+                                </ul>
+                                <p>L&rsquo;onglet <strong>Donn&eacute;es partag&eacute;es</strong> permet l&rsquo;acc&egrave;s &agrave; la description d&eacute;taill&eacute;e de l&rsquo;&eacute;tude&nbsp;:</p>
+                                <ul>
+                                    <li>justification th&eacute;orique</li>
+                                    <li>questions cliniques</li>
+                                    <li>objectifs</li>
+                                    <li>protocole exp&eacute;rimental</li>
+                                </ul>
+                                <p>&nbsp;</p>
+                            </div>
+                            <div role="tabpanel" class="tab-pane fade" id="tab_content33" aria-labelledby="profile-tab">
+                                <p>Nous vous proposons ici un d&eacute;roulement pratique du protocole. En dehors de la p&eacute;riode d&rsquo;entra&icirc;nement, ce calendrier est bien s&ucirc;r &agrave; titre indicatif et correspond &agrave; nos propres observations lors des passations.</p>
+                                <table>
+                                    <tbody>
+                                    <tr>
+                                        <td width="143">
+                                            <p>&nbsp;</p>
+                                        </td>
+                                        <td width="153">
+                                            <p>Bilan pr&eacute;-test</p>
+                                            <p><strong>T0</strong></p>
+                                        </td>
+                                        <td width="170">
+                                            <p>Entra&icirc;nement</p>
+                                        </td>
+                                        <td width="153">
+                                            <p>Bilan post-test</p>
+                                            <p><strong>T1</strong></p>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td width="143">
+                                            <p><strong>Dur&eacute;e&nbsp;: </strong></p>
+                                        </td>
+                                        <td width="153">
+                                            <p>1 session de 1h *</p>
+                                        </td>
+                                        <td width="170">
+                                            <p>5 semaines</p>
+                                            <p>&nbsp;</p>
+                                            <p>1 session quotidienne de 5 minutes</p>
+                                        </td>
+                                        <td width="153">
+                                            <p>1 session de 1h</p>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td width="143">
+                                            <p><strong>Dur&eacute;e totale&nbsp;: </strong>&nbsp;</p>
+                                        </td>
+                                        <td colspan="3" width="476">
+                                            <p>6 &agrave; 7 semaines selon le d&eacute;lai bilan/entra&icirc;nement</p>
+                                        </td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                                <p>&nbsp;</p>
+                                <p>* La passation des tests d&eacute;passant les 30 minutes imparties pour une s&eacute;ance, n&rsquo;oubliez pas de programmer une s&eacute;ance bilan plus longue&nbsp;!</p>
+                            </div>
+                            <div role="tabpanel" class="tab-pane fade" id="tab_content44" aria-labelledby="profile-tab">
+                                <p><a href="mailto:equipe.pr2m@gmail.com">equipe.pr2m@gmail.com</a> &nbsp;&nbsp;&nbsp;&nbsp;</p>
+                                <p>+ 33 (6) 45 40 36 39 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Nothelier Julie</p>
+                                <p>+ 33 (7) 77 38 10 61 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Giavelly Marion</p>
+                                <p>N&rsquo;h&eacute;sitez pas &agrave; nous contacter pour toute question ou toute difficult&eacute;.</p>
+                                <p>Nous vous r&eacute;pondrons dans les plus brefs d&eacute;lais.</p>
+
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 col-sm-6 col-xs-12">
+            <div class="x_panel">
+                <div class="x_title">
+                    <h2><i class="fa fa-info-circle"></i> Bienvenue sur PR2M !</h2>
                     <ul class="nav navbar-right panel_toolbox">
                         <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
                         </li>
@@ -52,56 +167,9 @@
                         <p>Suivez notre Guide d’utilisation et n’hésitez pas à nous contacter pour toute question.</p>
                         <p>Bonnes passations et à bientôt</p>
                         <small>- L’équipe PR2M</small>
-                        </div>
                     </div>
-
                 </div>
-            </div>
-        <div class="col-md-6 col-sm-6 col-xs-12">
-            <div class="x_panel">
-                <div class="x_title">
-                    <h2>Guide d'utilisation <small>Obtenez de l'aide facilement</small></h2>
-                    <ul class="nav navbar-right panel_toolbox">
-                        <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-                        </li>
 
-                    </ul>
-                    <div class="clearfix"></div>
-                </div>
-                <div class="x_content">
-
-                    <div class="col-xs-3">
-                        <!-- required for floating -->
-                        <!-- Nav tabs -->
-                        <ul class="nav nav-tabs tabs-left">
-                            <li class="active"><a href="#home" data-toggle="tab">Home</a>
-                            </li>
-                            <li><a href="#profile" data-toggle="tab">Profile</a>
-                            </li>
-                            <li><a href="#messages" data-toggle="tab">Messages</a>
-                            </li>
-                            <li><a href="#settings" data-toggle="tab">Settings</a>
-                            </li>
-                        </ul>
-                    </div>
-
-                    <div class="col-xs-9">
-                        <!-- Tab panes -->
-                        <div class="tab-content">
-                            <div class="tab-pane active" id="home">
-                                <p class="lead">Home tab</p>
-                                <p>Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua, retro synth master cleanse. Mustache cliche tempor, williamsburg carles vegan helvetica. Reprehenderit butcher retro keffiyeh dreamcatcher
-                                    synth. Cosby sweater eu banh mi, qui irure terr.</p>
-                            </div>
-                            <div class="tab-pane" id="profile">Profile Tab.</div>
-                            <div class="tab-pane" id="messages">Messages Tab.</div>
-                            <div class="tab-pane" id="settings">Settings Tab.</div>
-                        </div>
-                    </div>
-
-                    <div class="clearfix"></div>
-
-                </div>
             </div>
         </div>
     </div>
