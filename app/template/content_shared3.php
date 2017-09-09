95c40a12a0231436af713bb5de1dc0887d243f9a
WDelenclos <wdelenclos@gmail.com>
2017-09-09 17:22:19 +0200

    Fix

--- app/template/content_shared3.php
+++ app/template/content_shared3.php
@@ -19,7 +19,7 @@
                         <p>Selon nos questions cliniques et notre protocole d&rsquo;entra&icirc;nement &agrave; la d&eacute;nomination chronom&eacute;tr&eacute;e d&rsquo;images, nous souhaitons mesurer les effets sur&nbsp;:</p>
                         <p>&nbsp;1) l&rsquo;acc&egrave;s lexical,</p>
                         <p>&nbsp;2) les diff&eacute;rentes modalit&eacute;s de vitesse de traitement verbal et non verbal.</p>
-                        <h3>Crit&egrave;res d&rsquo;&eacute;valuation principaux :</h3>
+                        <h5>Crit&egrave;res d&rsquo;&eacute;valuation principaux :</h5>
                         <p>Nous &eacute;valuerons nos objectifs gr&acirc;ce aux r&eacute;sultats obtenus en vitesse et en pr&eacute;cision aux &eacute;preuves, avant et apr&egrave;s entra&icirc;nement.&nbsp;</p>
                         <p>La ligne de base nous permettra &eacute;galement de comparer les performances des deux groupes: contr&ocirc;le (CR) et exp&eacute;rimental (XP).&nbsp;</p>
                         <p>&nbsp;</p></div>
