6d1ea4e4bb921c711dc9b79043ad2d0bcc1969a6
WDelenclos <wdelenclos@gmail.com>
2017-10-24 14:52:27 +0200

    Fix

--- app/data/FilesUploads/resources/themes/material/default_footer.php
+++ app/data/FilesUploads/resources/themes/material/default_footer.php
@@ -3,7 +3,7 @@
 <div class="divider <?php echo $divider; ?>"></div>
 
 <div class="center-align <?php echo $textcolor; ?>">
-    <?php if ($supportdirectorylister) {?>Powered by, <a href="http://www.directorylister.com" class="<?php echo $color; ?>-text" target="_blank">Directory Lister</a>.<?php } ?>
-	<br class="hide-on-med-and-up"><?php if ($supportjustinkruit) {?>Material theme made by <a href="http://justinkruit.com" class="<?php echo $color; ?>-text" target="_blank">Justin Kruit</a><?php } ?>
+    <?php if ($supportdirectorylister) {?>Serveur PR2M <?php } ?>
+	<br class="hide-on-med-and-up"><?php if ($supportjustinkruit) {?>Serveur PR2M<?php } ?>
 </div></div>
 <?php } ?>
