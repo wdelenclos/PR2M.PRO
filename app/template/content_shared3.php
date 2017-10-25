bbe89caa60d6c332b7286b3ef192b4e222933465
WDelenclos <wdelenclos@gmail.com>
2017-10-25 18:26:26 +0200

    Fix contact

--- app/template/content_shared3.php
+++ app/template/content_shared3.php
@@ -7,7 +7,7 @@
         <div class="col-md-12">
             <div class="x_panel">
                 <div class="x_title">
-                    <h2>Objectifs</h2>
+                    <h2>A propos</h2>
                     <ul class="nav navbar-right panel_toolbox">
                         <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
                         </li>
@@ -15,14 +15,21 @@
                     <div class="clearfix"></div>
                 </div>
                 <div class="x_content">
+                    
                     <div class="col-md-12">
-                        <p>Selon nos questions cliniques et notre protocole d&rsquo;entra&icirc;nement &agrave; la d&eacute;nomination chronom&eacute;tr&eacute;e d&rsquo;images, nous souhaitons mesurer les effets sur&nbsp;:</p>
-                        <p>&nbsp;1) l&rsquo;acc&egrave;s lexical,</p>
-                        <p>&nbsp;2) les diff&eacute;rentes modalit&eacute;s de vitesse de traitement verbal et non verbal.</p>
-                        <h5>Crit&egrave;res d&rsquo;&eacute;valuation principaux :</h5>
-                        <p>Nous &eacute;valuerons nos objectifs gr&acirc;ce aux r&eacute;sultats obtenus en vitesse et en pr&eacute;cision aux &eacute;preuves, avant et apr&egrave;s entra&icirc;nement.&nbsp;</p>
-                        <p>La ligne de base nous permettra &eacute;galement de comparer les performances des deux groupes: contr&ocirc;le (CR) et exp&eacute;rimental (XP).&nbsp;</p>
-                        <p>&nbsp;</p></div>
+                       
+                
+
+                    <p class="lead">Contact</p>
+                                <hr>
+                                <p><strong>Email :</strong> <a href="mailto:equipe.pr2m@gmail.com">equipe.pr2m@gmail.com</a> &nbsp;&nbsp;&nbsp;&nbsp;</p>
+                                <p><strong>Nothelier Julie :</strong> + 33 (6) 45 40 36 39 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
+                                <p><strong>Giavelly Marion :</strong> + 33 (7) 77 38 10 61 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
+                                <p>N’hésitez pas à nous contacter pour toute question ou toute difficulté.</p>
+                                <p>Nous vous répondrons dans les plus brefs délais.</p>
+
+                            
+                    </div>
             </div>
         </div>
     </div>
