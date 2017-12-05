5838473a43c5dfcd271201b802039220b46b2254
root <root@vps418482.ovh.net>
2017-12-06 00:07:30 +0100

    Push serve

--- app/config.php
+++ app/config.php
@@ -5,14 +5,14 @@
 define('CONTACTMAIL','equipe.pr2m@gmail.com' );
 
 // Administrateur
-define('ADMINMAIL',[crc32('wdelenclos@gmail.com'),crc32('julie.nothelier@gmail.com')] );
+define('ADMINMAIL',[crc32('marion.giavelly@gmail.com'),crc32('wdelenclos@gmail.com'),crc32('julie.nothelier@gmail.com')] );
 define('MDP','125-ayN-92' );
 
 // BDD INFO
 define("DNS", 'mysql:host=localhost;');
 define("DBNAME",'dbname=pr2m'  );
 define('USERNAME','root');
-define('PASSWD', 'root');
+define('PASSWD', '125-ayN-92');
 
 // Durée de vie des connexions en secondes
 define('SESSIONEXPRIRE', 24*3600);
